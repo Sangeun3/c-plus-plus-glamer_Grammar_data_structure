5822f62e5138cebe96710385ab7bcfc22b961c4a
Sangeun3 <129152273+Sangeun3@users.noreply.github.com>
2025-08-25 13:25:32 +0900

    새로운 소스 파일 20250824_08.cpp 추가
    01_241223.vcxproj에 20250824_08.cpp 파일을 추가하고,
    모든 빌드 구성에서 제외되도록 설정했습니다.
    또한, 01_241223.vcxproj.filters에 새로운 필터
    소스 파일\얌얌구조 08를 추가하여 해당 파일을 포함시켰습니다.
    20250824_08.cpp 파일에는 재귀적으로 문자열을 생성하는
    test 함수와 main 함수가 포함되어 있으며, 코드에 대한
    주석도 추가되었습니다.

---
+++ 01_241223/20250824_08.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+// 다량의 ABC 세종류의 카드를 가지고 있다.
+// 이 중 3장의 카드를 뽑을 때 모든 조합을 출력하라
+// 단 같은 카드를 연속 2장 뽑으면 안된다.
+
+char path[5] = "";
+int arr[5] = { 5,7,1,2,3 };
+
+void test(int x)
+{
+	// 무한정 호출되ㅣ는 함수를
+	// 방지해야 하는게 급선무이다.
+	
+	//진입했다가 특정 구간에서 return 해주는 경우
+	/*if (path[0] =='A') 
+	{
+		return;
+	}*/
+	if (x >= 2 && path[x - 2] == path[x - 1])
+		return;
+
+	if (x == 3)
+	{
+		std::cout << path << std::endl;
+		return;
+	}
+	 
+	for (size_t i = 0; i < 3; i++)
+	{
+		//아예 진입을 안하는 경우
+		/*if (x == 0 && ('A' + i) == 'A')
+		{
+			continue;
+		}*/
+		path[x] = 'A' + i;
+		test(x + 1);
+		path[x] = 0;
+	}
+	
+}
+
+int main()
+{
+	test(0);
+
+	return 0;
+}
